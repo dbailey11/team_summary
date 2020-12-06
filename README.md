022f0344ac6d7810b61347670fb14be4cc07136e
Devyn Bailey <devynbailey11@yahoo.com>
2020-12-06 14:49:32 -0700

    user input renders html file

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 # team_summary
 
 # Pseudo
-* when user runs application on command line - presented with a set of questions - Inquirer
-* prompt user for info about the team manager and team members
-** name/email/ID/role
-** manager - office #
-** engineer - github
-** intern - school
-* can input any number of members - mix of engineers and interns
-* app must pass all unit tests - Jest
-* once quesitons are answered - the app creates an HTML file that displays a nicely formatted team roster
 
-** Don't understand unit tests - recommended to follow this workflow?
+- when user runs application on command line - presented with a set of questions - Inquirer
+- prompt user for info about the team manager and team members
+  ** name/email/ID/role
+  ** manager - office #
+  ** engineer - github
+  ** intern - school
+- can input any number of members - mix of engineers and interns
+<!-- * app must pass all unit tests - Jest -->
+- once quesitons are answered - the app creates an HTML file that displays a nicely formatted team roster
+
+<!-- * DONE
+** recommended to follow this workflow?
 --- run test
 --- create/update classes to pass a single test case
---- repeat
+--- repeat -->
+<!-- *** is this testing the class functions in the modules we create??? -->
 
-*** is this testing the class functions in the modules we create???
-
-*** In your HTML template files, you may want to add a placeholder character that helps your program identify where the dynamic markup begins and ends
-
+\*\*\* In your HTML template files, you may want to add a placeholder character that helps your program identify where the dynamic markup begins and ends
 
 EXAMPLES
 https://github.com/hoffman1200/Homework-10/blob/master/Develop/app.js
 
---- once npm install - have jest
+<!-- --- once npm install - have jest
 --- once you write code - then run test "npm run test"
---> runs test then tells you the results
---- want code to pass all the tests - then can run app.js/create it
+<!-- --> runs test then tells you the results -->
+<!-- --- want code to pass all the tests - then can run app.js/create it --> -->
--- app.js
+++ app.js
@@ -74,8 +74,7 @@ function buildTeam() {
         engineer();
       } else if (answer.memType === "Intern") {
         intern();
-      }
-      // else(generateTeam());
+      }else(generateTeam());
     });
 }
 
@@ -89,7 +88,7 @@ function manager() {
       },
     ])
     .then(function (answer) {
-      const engineer = new Engineer(
+      const manager = new Manager(
         answer.name,
         answer.id,
         answer.email,
@@ -176,23 +175,25 @@ function intern() {
         answer.school
       );
 
-      teamMem.push(engineer);
-      emptyId.push(answer.github);
+      teamMem.push(intern);
+      emptyId.push(answer.school);
 
       buildTeam();
       //   writeToFile("team.html", outputPath(answer));
     });
 }
 
-// function generateTeam() {
-
-// }
+function generateTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMem), "utf-8");
+}
 
 // function call to initialize program
 employee();
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -203,12 +204,6 @@ employee();
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
